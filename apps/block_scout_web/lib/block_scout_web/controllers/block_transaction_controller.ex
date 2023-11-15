1a6d9bf95abd5cb762c5cc052779fd95276a1e01
Nguyen Cuong <nguyencuong@Nguyens-MacBook-Pro-2.local>
2023-11-15 15:26:13 +0700

    privacy block

--- apps/block_scout_web/lib/block_scout_web/controllers/block_transaction_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/block_transaction_controller.ex
@@ -2,18 +2,29 @@ defmodule BlockScoutWeb.BlockTransactionController do
   use BlockScoutWeb, :controller
 
   import BlockScoutWeb.Chain,
-    only: [paging_options: 1, put_key_value_to_paging_options: 3, next_page_params: 3, split_list_by_page: 1]
+    only: [
+      paging_options: 1,
+      put_key_value_to_paging_options: 3,
+      next_page_params: 3,
+      split_list_by_page: 1
+    ]
 
   import Explorer.Chain, only: [hash_to_block: 2, number_to_block: 2, string_to_block_hash: 1]
 
   alias BlockScoutWeb.{Controller, TransactionView}
   alias Explorer.Chain
   alias Phoenix.View
+  alias BlockScoutWeb.Privacy.PrivacyVerify
 
-  {:ok, burn_address_hash} = Chain.string_to_address_hash("0x0000000000000000000000000000000000000000")
+  {:ok, burn_address_hash} =
+    Chain.string_to_address_hash("0x0000000000000000000000000000000000000000")
+
   @burn_address_hash burn_address_hash
 
-  def index(conn, %{"block_hash_or_number" => formatted_block_hash_or_number, "type" => "JSON"} = params) do
+  def index(
+        conn,
+        %{"block_hash_or_number" => formatted_block_hash_or_number, "type" => "JSON"} = params
+      ) do
     case param_block_hash_or_number_to_block(formatted_block_hash_or_number, []) do
       {:ok, block} ->
         full_options =
@@ -47,6 +58,20 @@ defmodule BlockScoutWeb.BlockTransactionController do
               )
           end
 
+        ## Verify wallet
+        address_verified = PrivacyVerify.wallet_verify(conn, nil)
+        wallet_login = PrivacyVerify.wallet_login_verify(conn)
+
+        wallet_login_hash =
+          if wallet_login == nil do
+            nil
+          else
+            {:ok, wallet_login_hash} = Chain.string_to_address_hash(wallet_login)
+            wallet_login_hash
+          end
+
+        ##
+
         items =
           transactions
           |> Enum.map(fn transaction ->
@@ -59,13 +84,31 @@ defmodule BlockScoutWeb.BlockTransactionController do
             transaction_with_transfers_filtered =
               Map.put(transaction, :token_transfers, token_transfers_filtered_by_block_hash)
 
-            View.render_to_string(
-              TransactionView,
-              "_tile.html",
-              transaction: transaction_with_transfers_filtered,
-              burn_address_hash: @burn_address_hash,
-              conn: conn
-            )
+            %Chain.Transaction{
+              from_address_hash: from_address_hash,
+              to_address_hash: to_address_hash
+            } = transaction
+
+            if address_verified == true ||
+                 (wallet_login_hash != nil &&
+                    (from_address_hash == wallet_login_hash ||
+                       to_address_hash == wallet_login_hash)) do
+              View.render_to_string(
+                TransactionView,
+                "_tile.html",
+                transaction: transaction_with_transfers_filtered,
+                burn_address_hash: @burn_address_hash,
+                conn: conn
+              )
+            else
+              View.render_to_string(
+                TransactionView,
+                "_empty.html",
+                transaction: transaction_with_transfers_filtered,
+                burn_address_hash: @burn_address_hash,
+                conn: conn
+              )
+            end
           end)
 
         json(
